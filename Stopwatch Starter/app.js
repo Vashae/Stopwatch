57baea9c21ebe7a22ae9443d7661b9cc5caba47c
Vashae <vashaeb2@gmail.com>
2025-01-07 12:06:14 -0500

    updated stopwatch

--- Stopwatch Starter/app.js
+++ Stopwatch Starter/app.js
@@ -1,51 +1,49 @@
 const timer__milliseconds = document.querySelector('.timer__milliseconds')
 const timer__seconds = document.querySelector('.timer__seconds')
-const timer__Minutes = document.querySelector('.timer__minutes')
+const timer__minutes = document.querySelector('.timer__minutes')
 
 
-let savedTime = 0
-let cancelId ;
+let savedTime=0
 let startTime;
-
+let cancelId;
 
 
 function startTimer (){
 startTime = Date.now()
+
     cancelId = requestAnimationFrame(updateTimer)
 }
 function stopTimer (){
-savedTime = savedTime + Date.now() - startTime
+    savedTime = savedTime + Date.now() - startTime
 cancelAnimationFrame(cancelId)
 }
 function resetTimer (){
-startTime = Date.now()
 savedTime = 0
-timer__milliseconds.innerHTML = '000'
- timer__seconds.innerHTML = '00'
-timer__Minutes.innerHTML = '00'
+startTime = Date.now()
+    timer__milliseconds.innerHTML = '000'
+    timer__seconds.innerHTML = '00'
+    timer__minutes.innerHTML = '00'
 }
 function updateTimer (){
-    let MilliElapsed = savedTime + Date.now() - startTime +50000 ;
-    let MilliText = MilliElapsed % 1000;
-    if (MilliText.toString().length === 1) {
-        MilliText = '0' + MilliText;
-    }
-    let secondsElapsed = MilliElapsed / 1000;
-    let secondsText = Math.floor(secondsElapsed % 60);
-    if (secondsText.toString().length === 1) {
-        secondsText = '0' + secondsText
-    }
-    
-let MinutesElapsed = secondsElapsed / 60
-let MinutesText = Math.floor(MinutesElapsed)
-if (MinutesText.toString().length < 2) {
-    MinutesText.toString().padStart(2 , '0')
+let millisElapsed = savedTime + Date.now() - startTime
+let millisText = millisElapsed % 1000
+if (millisText.toString().length === 2) {
+    millisText = '0' + millisText
+}
+let secondsElapsed = millisElapsed / 1000
+let secondsText = Math.floor(secondsElapsed % 60)
+if (secondsText.toString().length < 2) {
+    secondsText = secondsText.toString().padStart(2 , '0')
+}
+let minutesElapsed = secondsElapsed / 60
+let minutesText = Math.floor(minutesElapsed)
+if (minutesText.toString().length < 2) {
+    minutesText = minutesText.toString().padStart(2 , '0')
 }
 
-    
-    
-    timer__milliseconds.innerHTML = MilliText
-    timer__Minutes.innerHTML = MinutesText
-    timer__seconds.innerHTML = secondsText
-    cancelId = requestAnimationFrame(updateTimer)
+
+timer__milliseconds.innerHTML = millisText
+timer__seconds.innerHTML = secondsText
+timer__minutes.innerHTML = minutesText
+ cancelId = requestAnimationFrame(updateTimer)
 }
